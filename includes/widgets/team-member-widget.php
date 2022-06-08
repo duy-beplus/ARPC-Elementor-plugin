b99a1f58e0c342a6d7b03f2b0ac42c356ef79373
duy-beplus <duy.beplus@gmail.com>
2022-06-08 17:05:56 +0700

    Revert "Update Board and Leadership section"
    This reverts commit eb33b9ce03a9579db3a247a754905d7fef789592.

--- includes/widgets/team-member-widget.php
+++ includes/widgets/team-member-widget.php
@@ -272,6 +272,14 @@ class TeamMemberWidget extends Widget_Base
             ]
         );
         $this->end_controls_section();
+        // The board settings
+        $this->start_controls_section(
+            'board-section',
+            [
+                'label' => 'The Board',
+                'tab' => Controls_Manager::TAB_CONTENT
+            ]
+        );
         // Get query options
         $options = [];
         $posts = get_posts(array(
