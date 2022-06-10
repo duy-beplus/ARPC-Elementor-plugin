2bd1ff87b1d7b2158972ec443e9d089a5af55be7
tuanbeplus <tuan.beplus@gmail.com>
2022-06-10 08:42:52 +0700

    update postcodes filter

--- includes/widgets/assets/js/postcodes-filter-script.js
+++ includes/widgets/assets/js/postcodes-filter-script.js
@@ -1,29 +1,38 @@
 $(document).ready(function(){
-  $("#postcodes-number").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-
-  $("#postcodes-number").change( function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
+  function filter_postcode_au(number, state, tier) {
+         console.log(number, state, tier);
+        $.ajax({
+					type: 'POST',
+					url: ajaxObject.ajaxUrl,
+					data:{
+						'action' : 'load_postcodes_data',
+            'postcode'  : number,
+            'state'  : state,
+            'tier'  : tier,
+					},
+					success:function(response){
+              $('.postcodes-result-table').html(response);
+          }
+        });
+  }
+  $("#postcodes-number").on("keyup change", function() {
+        let number = $(this).val();
+        let state = $("#postcodes-states").val();
+        let tier = $("#postcodes-tiers").val();
+        filter_postcode_au(number, state, tier);
   });
 
   $("#postcodes-states").change( function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
+        let state = $(this).val();
+        let number = $("#postcodes-number").val();
+        let tier = $("#postcodes-tiers").val();
+        filter_postcode_au(number, state, tier);
   });
 
   $("#postcodes-tiers").change( function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
+        let tier = $(this).val();
+        let number = $("#postcodes-number").val();
+        let state = $("#postcodes-states").val();
+        filter_postcode_au(number, state, tier);
+      });
   });
-});
